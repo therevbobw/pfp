4197d1508c5ac4c3a69442636aa39fde3186524c
therevbobw <bob@bobjanet.demon.co.uk>
2014-09-30 20:44:46 +0100

    Just trying this...:q :quit
    :i

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ pfp
 
 Prayers For Places is for putting a prayer onto a mobile device when it enters a geofence.
 This may all take a while to get going. I am going to try phonegap.
+
+1.0.1 first attempt at splitting it
---
+++ myfun.js
@@ -0,0 +1,90 @@
+function onDeviceReady() {
+  // Now safe to use the PhoneGap API
+  //navigator.notification.alert("Starting", doLittle);
+  //var bong = new Media(bongsrc);
+  //bong.play;
+  //navigator.notification.vibrate(2000);
+  getLocation();
+}
+//var interval01 = setInterval(getLocation, 3000);
+function geo_error() {
+  alert("Sorry, no position available.\n"+error.message);
+}
+function getLocation()
+  {
+    //form0['mark'].value = "in";
+    if (navigator.geolocation)
+    {
+      navigator.geolocation.watchPosition(usePosition, geo_error, geo_options);
+    }
+  //else{x.innerHTML="Geolocation is not supported by this browser.";}
+    //form0['mark'].value = "out";
+  }
+function OpenPopup(i)
+  {
+//   if(isOpen == "false")
+//   {
+         //alert("Going in "+i); 
+         //popped = window.open(textlocs[i]);
+         navigator.notification.vibrate(800);
+         //navigator.notification.beep(1);
+         document.getElementById('stuff').src = textlocs[i];
+         //bong.play;
+         navigator.notification.alert("New prayer now", doLittle());
+         //$('#stuff').attr('src', textlocs[i]);
+//    }
+  } 
+function doLittle()
+  {
+  	//not sure what to do here yet...
+  }
+function usePosition(position)
+  {
+    //navigator.geolocation.clearWatch(watching);
+    thisTime = Date.now();
+    if ((thisTime - lastUpdateTime) > interval)
+    {
+    	lastUpdateTime = thisTime;
+	var myLatitude = position.coords.latitude;
+	var myLongitude = position.coords.longitude;
+	form0['lat'].value = myLatitude;
+    	form0['lon'].value = myLongitude;
+        then = now;
+    	now = Date.now();
+    	timegap = now-then;
+    	form0['mark'].value = timegap;
+    	anyOpen = "false";
+    	for (i = 0; i < nPlaces; i++) { 
+      		dy = (myLatitude - lats[i+1])*111300;
+      		dx = (myLongitude - longs[i+1])*111300*Math.cos(myLatitude*angConv);
+      		//d = Math.sqrt(dx*dx+dy*dy);
+      		dsq = dx*dx+dy*dy;
+      		if (dsq < tsq){
+        		anyOpen = "true";
+        		indexOfWanted = i+1;
+      		}
+    	}
+    	if (anyOpen == "false" && isOpen == "true"){
+        	isOpen = "false";
+        	//alert("Going out");
+        	//popped.close();
+        	//navigator.notification.beep(1);
+        	document.getElementById('stuff').src="null.html";
+        	//$('#stuff').attr('src', "null.html");
+    	}
+    	if (anyOpen == "false" && isOpen == "false"){
+        	// no action
+    	}
+    	if (anyOpen == "true" && isOpen == "false"){
+        	isOpen = "true";
+        	OpenPopup(indexOfWanted);
+    	}
+    	if (anyOpen == "true" && isOpen == "true"){
+        	/*TODO allow for (currently impossible) case where go from
+        	one point to another without passing through empty space
+        	currently no action in this case*/
+    	}
+    }
+
+	
+  }
