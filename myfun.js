5aef92a8467687da2ec19c47c6ad025efb73680e
therevbobw <bob@bobjanet.demon.co.uk>
2014-10-02 16:12:05 +0100

    Update myfun.js

--- myfun.js
+++ myfun.js
@@ -88,3 +88,9 @@ function usePosition(position)
 
 	
   }
+function fixiFrame () {
+   if ((android) && (androidVersion > 3)) {
+      iFrameContentHeight = document.getElementById('theiFrame').contentDocument.body.offsetHeight;
+      document.getElementById('theiFrame').style.height = iFrameContentHeight + 'px';
+   }
+}
