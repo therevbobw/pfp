0d6ecf9d9d948e3fe4f65ae09b115f0d8c6b13f2
therevbobw <bob@bobjanet.demon.co.uk>
2014-10-10 07:54:58 +0100

    Update myfun.js

--- myfun.js
+++ myfun.js
@@ -180,10 +180,10 @@ function goForward(){
 	window.history.forward();
 }
 function closeMessage() {
-	alert("closing...");
+	//alert("closing...");
 	el = document.getElementById("messageSpace");
 	el.style.visibility = "hidden";
-	alert("should be closed now!");
+	//alert("should be closed now!");
 }
 function showMessage(messageText) {
 	el = document.getElementById("messageSpace");
