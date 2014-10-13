9e4dd69832b758604fdf2b33be738a76bb73715c
therevbobw <bob@bobjanet.demon.co.uk>
2014-10-13 15:25:51 +0100

    Update startup.js

--- startup.js
+++ startup.js
@@ -29,7 +29,7 @@ myAccuracy = 999999;
   //also these may depend on type of journey. Consider revising if I do road/rail and include in
   //the 'by route' variables. Also later could include a 'settings' option also to specify km/miles TODO
 mileageUpdateDistance = 50; //metres
-mileageUpdateAccuracyRatio = 0.3;
+mileageUpdateAccuracyRatio = 0.25;
 mileageAccuracyThreshold = mileageUpdateDistance * mileageUpdateAccuracyRatio;
 matsq = mileageAccuracyThreshold * mileageAccuracyThreshold;
 metresToMiles = 0.000621371192;
