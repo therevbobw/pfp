59ad9c17680b7cc4525ef4531263bbef7ae60047
therevbobw <bob@bobjanet.demon.co.uk>
2014-10-13 14:51:15 +0100

    Update startup.js

--- startup.js
+++ startup.js
@@ -24,6 +24,7 @@ infoString = "<p class='mileage'>No fix yet</p><hr>";
 aggregateDistance = 0;
 lastMileageLatitude = 999;
 lastMileageLongitude = 999;
+myAccuracy = 999999;
   //play the next one by ear - too much and you cut off corners - too little and you get 'noise'
   //also these may depend on type of journey. Consider revising if I do road/rail and include in
   //the 'by route' variables. Also later could include a 'settings' option also to specify km/miles TODO
